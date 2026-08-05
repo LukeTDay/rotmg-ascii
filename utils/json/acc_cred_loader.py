b9136ac4d58f6fd57a2580270fe711b052a21768
LukeTday <luke.day@gmail.com>
2026-08-05 15:38:40 -0400

    Credential loader for storing accounts to log into.

---
+++ utils/json/acc_cred_loader.py
@@ -0,0 +1,8 @@
+from typing import Dict, List
+import json
+
+def credential_loader() -> List[Dict[str,str]]:
+    with open("credentials/account_credentials.json", "r", encoding="utf-8") as f:
+        accounts = json.load(f)
+
+    return accounts
