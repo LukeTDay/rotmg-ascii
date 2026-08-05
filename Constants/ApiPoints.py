0ab29cb0f21cb5b3fbe0722c9c802bdceb1509ad
LukeTday <luke.day@gmail.com>
2026-08-05 15:53:10 -0400

    Copying constants folder from pyrelay for the API endpoints folder

---
+++ Constants/ApiPoints.py
@@ -0,0 +1,17 @@
+
+VERSION = "https://static.drips.pw/rotmg/production/current/version.txt"
+EQUIP = "https://static.drips.pw/rotmg/production/current/xml/Equip.xml"
+VERIFY = "https://www.realmofthemadgod.com/account/verify"
+VERIFYTOKEN = "https://www.realmofthemadgod.com/account/verifyAccessTokenClient"
+CHAR = "https://www.realmofthemadgod.com/char/list"
+SERVERS = "https://www.realmofthemadgod.com/account/servers"
+
+launcherHeaders = {
+    "User-Agent": "UnityPlayer/2021.3.16f1 (UnityWebRequest/1.0, libcurl/7.84.0-DEV)",
+    "X-Unity-Version": "2021.3.16f1"
+    }
+
+exaltHeaders = {
+    "User-Agent": "UnityPlayer/2021.3.16f1 (UnityWebRequest/1.0, libcurl/7.84.0-DEV)",
+    "X-Unity-Version": "2021.3.16f1"
+    }
