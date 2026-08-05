b81ae9d3e5d4d1e0417f0505243d94bb05f758ad
LukeTday <luke.day@gmail.com>
2026-08-05 16:12:47 -0400

    Finished getting acces token and client token based off of PyRelays Client.py class

--- Utils/json/accCredLoader.py
+++ Utils/json/accCredLoader.py
@@ -2,7 +2,7 @@ from typing import Dict, List
 import json
 
 def credential_loader() -> List[Dict[str,str]]:
-    with open("credentials/account_credentials.json", "r", encoding="utf-8") as f:
+    with open("Credentials/account_credentials.json", "r", encoding="utf-8") as f:
         accounts = json.load(f)
 
     return accounts
