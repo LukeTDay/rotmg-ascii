f9447feb76abc51b172cf49073e871e1981907e1
LukeTday <luke.day@gmail.com>
2026-08-07 17:43:49 -0400

    Safeguraded parseServersXML() in case an XML Server  is sent with out a Name or Dns field. Should realistically never happen

--- Utils/XML/parserServersXML.py
+++ Utils/XML/parserServersXML.py
@@ -7,11 +7,13 @@ def parseServersXML(r : requests.models.Response):
     root = et.fromstring(xmlText)
     serversDict : Dict[str,str] = {}
     for server in root.findall("Server"):
-        name  = server.find("Name").text
-        dns   = server.find("DNS").text
-        if name is None:
+        nameElem = server.find("Name")
+        dnsElem  = server.find("DNS")
+        if nameElem is None or dnsElem is None:
             continue
-        if dns is None:
+        name = nameElem.text
+        dns  = dnsElem.text
+        if name is None or dns is None:
             continue
         serversDict[name] = dns
     return serversDict
