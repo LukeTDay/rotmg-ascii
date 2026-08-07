34a2e9de9861b247e64306046640e48ed8531f49
LukeTday <luke.day@gmail.com>
2026-08-07 15:40:07 -0400

    Added API end points from https://github.com/rotmg-network/appengine-tool/blob/main/src/endpointCatalog.ts

--- Constants/ApiPoints.py
+++ Constants/ApiPoints.py
@@ -1,10 +1,50 @@
 
 VERSION = "https://static.drips.pw/rotmg/production/current/version.txt"
 EQUIP = "https://static.drips.pw/rotmg/production/current/xml/Equip.xml"
+
+# account/*
 VERIFY = "https://www.realmofthemadgod.com/account/verify"
 VERIFYTOKEN = "https://www.realmofthemadgod.com/account/verifyAccessTokenClient"
-CHAR = "https://www.realmofthemadgod.com/char/list"
 SERVERS = "https://www.realmofthemadgod.com/account/servers"
+REGISTER = "https://www.realmofthemadgod.com/account/register"
+CHANGEPASSWORD = "https://www.realmofthemadgod.com/account/changePassword"
+FORGOTPASSWORD = "https://www.realmofthemadgod.com/account/forgotPassword"
+ISEMAILVERIFIED = "https://www.realmofthemadgod.com/account/isEmailVerified"
+SENDVERIFYEMAIL = "https://www.realmofthemadgod.com/account/sendVerifyEmail"
+GETCREDITS = "https://www.realmofthemadgod.com/account/getCredits"
+OWNEDSKINS = "https://www.realmofthemadgod.com/account/ownedSkins"
+CLAIMLOGINREWARD = "https://www.realmofthemadgod.com/account/claimLoginReward"
+
+# char/*
+CHAR = "https://www.realmofthemadgod.com/char/list"
+CHARFAME = "https://www.realmofthemadgod.com/char/fame"
+CHARDELETE = "https://www.realmofthemadgod.com/char/delete"
+
+# serverStatus/*
+SERVERSTATUS = "https://www.realmofthemadgod.com/serverStatus/getServerStatus"
+
+# friends/*
+FRIENDSLIST = "https://www.realmofthemadgod.com/friends/getList"
+FRIENDREQUESTS = "https://www.realmofthemadgod.com/friends/getRequests"
+
+# guild/*
+GUILDBOARD = "https://www.realmofthemadgod.com/guild/getBoard"
+GUILDMEMBERS = "https://www.realmofthemadgod.com/guild/listMembers"
+
+# missions/*
+MISSIONS = "https://www.realmofthemadgod.com/missions/getPlayerMissions"
+
+# mysterybox/*
+MYSTERYBOX = "https://www.realmofthemadgod.com/mysterybox/getBoxes"
+
+# news/*
+NEWS = "https://www.realmofthemadgod.com/news/list"
+
+# dailyLogin/*
+DAILYLOGIN = "https://www.realmofthemadgod.com/dailyLogin/fetchCalendar"
+
+# package/*
+PACKAGE = "https://www.realmofthemadgod.com/package/getPackages"
 
 launcherHeaders = {
     "User-Agent": "UnityPlayer/2021.3.16f1 (UnityWebRequest/1.0, libcurl/7.84.0-DEV)",
