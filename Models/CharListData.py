6009ebe5ce25b318261efe55eeb5903f08e89824
LukeTday <luke.day@gmail.com>
2026-08-07 21:27:00 -0400

    Added CharListData for when parsing the CharList.xml

---
+++ Models/CharListData.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class CharListData():
+    def __init__(self,
+                 charID : int,
+                 objectType : int,
+                 isSeasonal : bool,
+                 currentFame : int,
+                 currentLevel : int,
+                 equipmentList : List[int]) -> None:
+        self.charID = charID
+        self.objectType = objectType
+        self.isSeasonal = isSeasonal
+        self.currentFame = currentFame
+        self.currentLevel = currentLevel
+        self.equipmentList = equipmentList
