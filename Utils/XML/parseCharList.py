baaa9f272303a327dc993fb721e968098c726ddf
LukeTday <luke.day@gmail.com>
2026-08-07 21:28:42 -0400

    Added ParseCharListt(). Walks through the xml and stores charID, objectType, isSeasonal, currentFame, currentLevel, equipmentList. This information is stored in a charListData object and the list of all the chars is stored in the context.

---
+++ Utils/XML/parseCharList.py
@@ -0,0 +1,49 @@
+from Models.CharListData import CharListData
+import requests
+import xml.etree.ElementTree as et
+
+def parseCharList(r : requests.models.Response):
+    xmlText = r.text
+    root = et.fromstring(xmlText)
+
+    charList = []
+    for char in root.findall("Char"):
+        charIDElement = char.get("id")
+        objectTypeElement = char.find("ObjectType")
+        isSeasonalElement = char.find("Seasonal")
+        currentFameElement = char.find("CurrentFame")
+        levelElement = char.find("Level")
+        equipmentElement = char.find("Equipment")
+
+        if (charIDElement is None or objectTypeElement is None or isSeasonalElement is None
+                or currentFameElement is None or levelElement is None or equipmentElement is None):
+            continue
+
+        objectTypeText = objectTypeElement.text
+        isSeasonalText = isSeasonalElement.text
+        currentFameText = currentFameElement.text
+        levelText = levelElement.text
+        equipmentText = equipmentElement.text
+
+        if (objectTypeText is None or isSeasonalText is None or currentFameText is None
+                or levelText is None or equipmentText is None):
+            continue
+
+        charID = int(charIDElement)
+        objectType = int(objectTypeText)
+        isSeasonal = True if isSeasonalElement.text == "True" else False
+        currentFame = int(currentFameText)
+        currentLevel = int(levelText)
+        equipmentList = []
+        for equipment in equipmentText.split(","):
+            equipmentList.append(int(equipment))
+
+        charList.append(CharListData(
+            charID=charID,
+            objectType=objectType,
+            isSeasonal=isSeasonal,
+            currentFame=currentFame,
+            currentLevel=currentLevel,
+            equipmentList=equipmentList
+        ))
+    return charList
